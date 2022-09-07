46a9857fe6a32ee2bf69492df382dbee615df9ce
Devang Solanki <75718583+Devang-Solanki@users.noreply.github.com>
2022-09-07 13:40:23 +0530

    Updated api calls and bypass cloudflare

--- recon-cloud.sh
+++ recon-cloud.sh
@@ -12,12 +12,12 @@ Help()
    # Display Help
    echo -e "${Yellow}[*] A bash script for scanning AWS public cloud footprint and getting suddomains, service name, cname and region from recon.cloud.${NC}"
    echo
-   echo -e "${Green}Syntax: ./subdomain-enum.sh${NC} ${Blue}[-d|h|]${NC}"
+   echo -e "${Green}Syntax: ./recon-cloud.sh${NC} ${Blue}[-d|h|]${NC}"
    echo -e "${Green}options:${NC}"
    echo -e "${Red}d :${NC}     ${Blue}Takes domain name as input.${NC}"
    echo -e "${Red}h :${NC}     ${Blue}Print this Help.${NC}"
    echo
-   echo -e "${Green}example ./subdomain-enum.sh -d example.org${NC}"
+   echo -e "${Green}example ./recon-cloud.sh -d example.org${NC}"
 }
 
 while getopts "hd:" option; do
@@ -36,30 +36,28 @@ done
 # Function for getting the list of domains from recon.cloud #
 #############################################################
 
+UserAgent="Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"
+
 Recon_cloud()
 {
-    Domain=$1
-    echo -e "${Blue}Requesting subdomain for:${NC} $Domain"
-    curl -s "https://recon.cloud/api/search?domain=$Domain" --output "/tmp/ans_$Domain.json"
+    echo -e "${Blue}Scanning AWS, Azure and GCP public cloud footprint:${NC} $Domain"
+    Ans=$(curl -A $UserAgent -s "https://recon.cloud/api/search?domain=$Domain")
 
-    Req_id=$(jq -r '.request_id' "/tmp/ans_$Domain.json")
-    Len=$(jq '.assets.assets | length' "/tmp/ans_$Domain.json")
-
-    if [[ $Req_id = "None"  ]]
-    then
-        if [ $Len -gt 0 ]
+    Req_id=$(jq -r '.request_id' <<< $Ans)
+    Len=$(jq '.cloud_assets_list | length' <<< $Ans)
+    if [ $Len -gt 0 ]
         then
-            jq -r '.assets.assets[] | .domain + " " + .service + " "  + .region + " "  + .cname' "/tmp/ans_$Domain.json" | tee -a recon.cloud.txt
-        fi
+            jq -r '.cloud_assets_list[] | .domain + " " + .service + " "  + .region + " "  + .cname' <<< $Ans | tee -a recon.cloud.txt
+            exit
     else
         while true; do
-            curl -s "https://recon.cloud/api/get_status?request_id=$Req_id" --output "/tmp/status_$Domain.json"
-            Status=$(jq -r '.status.stage' "/tmp/status_$Domain.json")
+            Req_Status=$(curl -A $UserAgent -s "https://recon.cloud/api/get_status?request_id=$Req_id")
+            Status=$(jq -r '.step' <<< $Req_Status)
             echo -ne "${Blue}Status:${NC} $Status\\r"
             if [ $Status = "finished" ]
             then
-            curl -s "https://recon.cloud/api/get_results?request_id=$Req_id" --output "/tmp/result_$Domain.json"
-            jq -r '.assets[] | .domain + " " + .service + " "  + .region + " "  + .cname' "/tmp/result_$Domain.json" | tee -a recon.cloud.txt
+            Result=$(curl -A $UserAgent -s "https://recon.cloud/api/get_results?request_id=$Req_id")
+            jq -r '.cloud_assets_list[] | .domain + " " + .service + " "  + .region + " "  + .cname' <<< $Result | tee -a recon.cloud.txt
             break
             fi
             sleep 3
